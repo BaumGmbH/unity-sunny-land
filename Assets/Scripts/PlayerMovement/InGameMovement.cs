6fb50900c2e1259a6577a0ce04808b77aaff4368
psyGamer <psygamer21@gmail.com>
2022-05-21 20:09:51 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerMovement/InGameMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+            
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    public float runSpeed = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("Jump", true);
+        }    
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+    }
+
+    public void OnCrouch(bool IsCrouching)
+    {
+        animator.SetBool("Crouch", IsCrouching);
+    }
+
+    // Moves the character
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
